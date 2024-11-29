23d163671220069ed52dd454340249494feb1bd2
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-29 16:52:39 +0100

    aggiunta tags ai post

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,6 +2,7 @@ const defaultImgPath = "https://placehold.co/600x400?text=Sono+un+placeholder";
 import Button from "./button";
 
 export default function Card({ id, title, image, content, tags, published }) {
+	const tagsPost = tags.join(" ");
 	return (
 		<div className="card">
 			<img
@@ -12,6 +13,7 @@ export default function Card({ id, title, image, content, tags, published }) {
 			<div className="card-body">
 				<h2 className="card-title">{title}</h2>
 				<p className="card-text">{content}</p>
+				<p className="card-text">Tags: {tagsPost}</p>
 				<Button />
 			</div>
 		</div>
