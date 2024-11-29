bc02f07bf4a3cea83efac9adc9605f6343737822
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-29 15:40:32 +0100

    aggiunta placeholder ai post

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,17 @@ export default function Main() {
 	return (
 		<main>
 			<div className="container">
-				<Card />
+				{posts.map((post) => (
+					<Card
+						key={post.id}
+						id={post.id}
+						title={post.title}
+						image={post.image}
+						content={post.content}
+						tags={post.tags}
+						published={post.published}
+					/>
+				))}
 			</div>
 		</main>
 	);
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,22 +1,17 @@
-import image from "../assets/image_post.jpg";
+const defaultImgPath = "https://placehold.co/600x400?text=Sono+un+placeholder";
 import Button from "./button";
 
-export default function Card() {
+export default function Card({ id, title, image, content, tags, published }) {
 	return (
 		<div className="card">
 			<img
-				src={image}
+				src={image || defaultImgPath}
 				className="card-img-top img-fluid mx-auto"
 				alt="montagna"
 			/>
 			<div className="card-body">
-				<h2 className="card-title">Titolo del Post</h2>
-				<p className="card-text">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, ad
-					magnam delectus tempore reprehenderit quasi eveniet, sapiente eos
-					aliquid iste similique est ipsum repellat velit repellendus
-					perferendis rem harum adipisci.
-				</p>
+				<h2 className="card-title">{title}</h2>
+				<p className="card-text">{content}</p>
 				<Button />
 			</div>
 		</div>
