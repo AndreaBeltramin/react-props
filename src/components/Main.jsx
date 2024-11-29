272eac7aed13bb9318c7c8612f7c83aeedc3dc75
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-29 15:45:11 +0100

    filtro post published true

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,17 +5,19 @@ export default function Main() {
 	return (
 		<main>
 			<div className="container">
-				{posts.map((post) => (
-					<Card
-						key={post.id}
-						id={post.id}
-						title={post.title}
-						image={post.image}
-						content={post.content}
-						tags={post.tags}
-						published={post.published}
-					/>
-				))}
+				{posts
+					.filter((post) => post.published == true)
+					.map((post) => (
+						<Card
+							key={post.id}
+							id={post.id}
+							title={post.title}
+							image={post.image}
+							content={post.content}
+							tags={post.tags}
+							published={post.published}
+						/>
+					))}
 			</div>
 		</main>
 	);
