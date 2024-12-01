e9fa3f7dcb5cca29ec4f1dc9651888e85a3cb5bb
AndreaBeltramin <andreabeltra99@gmail.com>
2024-12-01 11:05:33 +0100

    filtro array di tag nella sezione dei tag

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,19 @@
 import { posts } from "../data/posts";
 
 export default function Navbar() {
+	const newArray = posts.flatMap((post) => post.tags);
+	//console.log(newArray);
+
+	const arrayTags = newArray.filter(
+		(value, index, self) => self.indexOf(value) === index
+	);
+	//console.log(arrayTags);
+
 	return (
 		<>
-			{posts
-				// .filter(
-				// 	(post) =>
-
-				// )
-				.map((post) => (
-					<li key={post.id}>{post.tags.join(" ")}</li>
-				))}
+			{arrayTags.map((tag, index) => (
+				<li key={index}>{tag}</li>
+			))}
 		</>
 	);
 }
